f93c972f40d7b9c3fad0e48fcaae6832310d837a
Nick Dyer <nbdyer@gmail.com>
2016-04-07 16:21:29 +0100

    Typo in loop to allow 53 loops

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Game {
 
     public void run(Display display, Board board) {
-        for (int i = 0; i < 53; i++) {
+        for (int i = 0; i < 5; i++) {
             display.showBoard(board);
             display.processMark(new Scanner(System.in), board, 'X');
             display.showBoard(board);
