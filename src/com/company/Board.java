eddf5287565244ec1cd6678f1dab82a281bfd060
Nick Dyer <nbdyer@gmail.com>
2016-04-18 09:21:49 +0100

    Adds DumpComputerTest from other branch

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Board {
 
     private List<Character> cells;
-    private char winningMark;
 
     public Board() {
         this.cells = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9');
---
+++ test/com/company/DumbComputerTest.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DumbComputerTest {
+
+    @Test
+    public void canMarkTheBoard() {
+        DumbComputer tron = new DumbComputer();
+        Board board = new Board();
+        tron.setMark('O');
+        tron.markBoard(3, board);
+        assertEquals('O', board.getMarkAt(3));
+    }
+
+    @Test
+    public void canGenerateValidMoveDecisionOnEmptyBoard() {
+        DumbComputer hal9000 = new DumbComputer();
+        Board board = new Board();
+        assertTrue(board.isCellEmpty(hal9000.generateMove(board)));
+    }
+
+    @Ignore
+    @Test
+    public void canGenerateValidMoveDecisionOnPartiallyFullBoard() {
+        DumbComputer hal9000 = new DumbComputer();
+        Board board = new Board();
+        for(int i=1;i<9;i++) {
+            board.mark(i, 'X');
+        }
+        assertTrue(board.isCellEmpty(0));
+        assertEquals(0, hal9000.generateMove(board));
+    }
+}
