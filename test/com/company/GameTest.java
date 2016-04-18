4a5e844d676723c267c05e07c47337761dccd26e
Nick Dyer <nbdyer@gmail.com>
2016-04-18 16:03:22 +0100

    100% coverage in game class, run method now works, although should be extracted

--- test/com/company/GameTest.java
+++ test/com/company/GameTest.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -9,7 +14,8 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
 
     private Game game;
-    private UserInterfaceSpy display;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private UserInterfaceSpy ui;
     private BoardSpy board;
     private Human player1;
     private Human player2;
@@ -17,51 +23,54 @@ public class GameTest {
     @Before
     public void setUp() {
         game = new Game();
-        display = new UserInterfaceSpy();
+        ui = new UserInterfaceSpy();
         board = new BoardSpy();
         player1 = new Human();
         player2 = new Human();
     }
 
-//    @Test
-//    public void canStartItself() {
-//        game.run(display, board, player1, player2);
-//        assertTrue(display.wasShowBoardCalled);
-//        assertTrue(display.wasProcessMarkCalled);
-//    }
+    @Test
+    public void canStartItself() {
+        DumbComputer comp1 = new DumbComputer();
+        DumbComputer comp2 = new DumbComputer();
+        Board board = new Board();
+        UserInterface newui = new UserInterface(new Scanner(""), new PrintStream(outContent));
+        game.run(newui, board, comp1, comp2);
+        assertTrue(game.isOver(board));
+    }
 
     @Test
     public void knowsWhenTheGameIsOverDueToDraw() {
         board.setADraw(true);
-        game.run(display, board, player1, player2);
+        game.run(ui, board, player1, player2);
         assertTrue(game.isOver(board));
     }
 
     @Test
     public void knowsWhenTheGameIsOverDueToWinCondition() {
         board.setHasAWinner(true);
-        game.run(display, board, player1, player2);
+        game.run(ui, board, player1, player2);
         assertTrue(game.isOver(board));
     }
 
     @Test
     public void gameWillCallDrawGameOverStatement() {
         board.setADraw(true);
-        game.run(display, board, player1, player2);
-        assertTrue(display.wasAnnounceDrawCalled);
+        game.run(ui, board, player1, player2);
+        assertTrue(ui.wasAnnounceDrawCalled);
     }
 
     @Test
     public void gameWillCallWinGameOverStatement() {
         board.setHasAWinner(true);
-        game.run(display, board, player1, player2);
-        assertTrue(display.wasAnnounceWinCalled);
+        game.run(ui, board, player1, player2);
+        assertTrue(ui.wasAnnounceWinCalled);
     }
 
     @Test
     public void gameKnowsWhosTurnItIs() {
         game.setUp(player1, player2);
-        game.promptTurn(game.getCurrentPlayer(), board, display);
+        game.promptTurn(game.getCurrentPlayer(), board, ui);
         assertEquals(player2, game.getCurrentPlayer());
     }
 }
