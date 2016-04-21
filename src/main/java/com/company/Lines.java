d72eee184c3d59e78828d848797ea3cf0738b780
Nick Dyer <nbdyer@gmail.com>
2016-04-21 17:47:04 +0100

    Refactors lines class further, renaming and method extraction

--- src/main/java/com/company/Lines.java
+++ src/main/java/com/company/Lines.java
@@ -20,6 +20,14 @@ class Lines {
         this.width = (int) Math.sqrt(size);
     }
 
+    public List<List<Mark>> possibleCombinations() {
+        List<List<Mark>> combinedList = new ArrayList<>();
+        combinedList.addAll(rows());
+        combinedList.addAll(columns());
+        combinedList.addAll(diagonals());
+        return combinedList;
+    }
+
     protected List<List<Mark>> rows() {
         List<List<Mark>> rows = new ArrayList<>();
         for (int i=0;i<3;i++) {
@@ -38,17 +46,20 @@ class Lines {
         return transpose(rows());
     }
 
-    private <T> List<List<T>> transpose(List<List<T>> table) {
-        List<List<T>> ret = new ArrayList<List<T>>();
-        final int N = table.get(0).size();
-        for (int i = 0; i < N; i++) {
-            List<T> col = new ArrayList<T>();
-            for (List<T> row : table) {
-                col.add(row.get(i));
-            }
-            ret.add(col);
+    private List<List<Mark>> transpose(List<List<Mark>> rows) {
+        List<List<Mark>> columns = new ArrayList<>();
+        for (int i = 0; i < width; i++) {
+            columns.add(makeColumn(rows, i));
         }
-        return ret;
+        return columns;
+    }
+
+    private List<Mark> makeColumn(List<List<Mark>> rows, int i) {
+        List<Mark> column = new ArrayList<>();
+        for (List<Mark> row : rows) {
+            column.add(row.get(i));
+        }
+        return column;
     }
 
     protected List<List<Mark>> diagonals() {
@@ -58,65 +69,43 @@ class Lines {
         return diagonals;
     }
 
-    private List<Mark> rightDiagonal() {
-        List<List<Mark>> rows = new ArrayList<>();
-        List<Mark> row1 = cells.subList(0, 3);
-        List<Mark> row2 = cells.subList(3, 6);
-        List<Mark> row3 = cells.subList(6, 9);
-        rows.add(row1);
-        rows.add(row2);
-        rows.add(row3);
+    private List<Mark> leftDiagonal() {
+        List<List<Mark>> rows = rows();
 
+        List<Mark> leftDiagonal = new ArrayList<>();
+        for(int i=0;i<width;i++) {
+            leftDiagonal.add(rows.get(i).get(i));
+        }
+        return leftDiagonal;
+    }
+
+    private List<Mark> rightDiagonal() {
+        List<List<Mark>> rows = rows();
         flipBoard(rows);
 
         List<Mark> right = new ArrayList<>();
-        for(int i=0;i<3;i++) {
+        for(int i=0;i<width;i++) {
             right.add(rows.get(i).get(i));
         }
-        flipBoard(rows);
         return right;
     }
 
     private void flipBoard(List<List<Mark>> rows) {
-        for(int i=0;i<3;i++) {
+        for(int i=0;i<width;i++) {
             Collections.reverse(rows.get(i));
         }
     }
 
-    private List<Mark> leftDiagonal() {
-        List<List<Mark>> rows = new ArrayList<>();
-        List<Mark> row1 = cells.subList(0, 3);
-        List<Mark> row2 = cells.subList(3, 6);
-        List<Mark> row3 = cells.subList(6, 9);
-        rows.add(row1);
-        rows.add(row2);
-        rows.add(row3);
-
-        List<Mark> left = new ArrayList<>();
-        for(int i=0;i<3;i++) {
-            left.add(rows.get(i).get(i));
-        }
-        return left;
-    }
-
-    public List<List<Mark>> possibleCombinations() {
-        List<List<Mark>> combinedList = new ArrayList<>();
-        combinedList.addAll(rows());
-        combinedList.addAll(columns());
-        combinedList.addAll(diagonals());
-        return combinedList;
-    }
-
     public Mark getWinningMark() {
-        lineExistsWithMatchingElements();
+        checkLinesForAllMatchingElements();
         return winningMark;
     }
 
     public boolean hasAWinner() {
-        return lineExistsWithMatchingElements();
+        return checkLinesForAllMatchingElements();
     }
 
-    private boolean lineExistsWithMatchingElements() {
+    private boolean checkLinesForAllMatchingElements() {
         for (List<Mark> line : possibleCombinations()) {
             if (hasAllMatchingElements(line)) {
                 winningMark = line.get(0);
