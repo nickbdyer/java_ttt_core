4be3d16fd262920fae1213fda728c9729df0042b
Nick Dyer <nbdyer@gmail.com>
2016-04-27 15:36:40 +0100

    Perfect computer will win if possible, and will choose center if other player went corner first

--- src/main/java/com/company/PerfectComputer.java
+++ src/main/java/com/company/PerfectComputer.java
@@ -8,7 +8,7 @@ import static com.company.Mark.*;
 public class PerfectComputer implements Player{
 
     private Mark mark;
-    private Map<Integer, Double> scored_moves;
+    private Map<Integer, Double> scoredMoves;
     private Map.Entry<Integer, Double> bestMove;
 
     public PerfectComputer(Mark mark) {
@@ -22,43 +22,49 @@ public class PerfectComputer implements Player{
 
     @Override
     public int choosePosition(UserInterface userInterface, Board board) {
-        if (board.isEmpty()) {
-            List<Integer> corners = Arrays.asList(0, 2, 6, 8);
-            Collections.shuffle(corners);
-            return corners.get(0);
-        }
-        scored_moves = new HashMap<Integer, Double>();
-        bestMove = null;
-        negamax(board, 0, -10, 10, 1);
-        findBestMove();
+        if (board.isEmpty()) return chooseRandomCorner();
+        resetMoveCalculations();
+        makeMoveCalculations(board);
         return bestMove.getKey();
     }
 
-    private void findBestMove() {
-         for (Entry<Integer,Double> entry : scored_moves.entrySet()) {
-            if (bestMove == null || entry.getValue().compareTo(bestMove.getValue()) > 0) {
-                bestMove = entry;
-            }
-        }
+    private void makeMoveCalculations(Board board) {
+        negamax(board, 0, -10, 10, 1);
+        findBestMove();
+    }
+
+    private void resetMoveCalculations() {
+        scoredMoves = new HashMap<>();
+        bestMove = null;
+    }
+
+    private int chooseRandomCorner() {
+        List<Integer> corners = Arrays.asList(0, 2, 6, 8);
+        Collections.shuffle(corners);
+        return corners.get(0);
     }
 
     private double negamax(Board board, int depth, double α, double β, int color) {
         if (board.isFull() || board.hasWinner()) return color * score(board, depth);
         double bestValue = -10;
         for (int cell : board.availableMoves()) {
-            if (color == 1) {
-                board.mark(cell, getMark());
-            } else {
-                board.mark(cell, opponent_symbol());
-            }
+            markCell(board, color, cell);
             double val = -negamax(board, depth + 1, -β, -α, -color);
             board.mark(cell, EMPTY);
             bestValue = Collections.max(Arrays.asList(bestValue, val));
-            if (depth == 0) scored_moves.put(cell, bestValue);
+            if (depth == 0) scoredMoves.put(cell, bestValue);
             α = Collections.max(Arrays.asList(α, bestValue));
             if (α >= β) break;
         }
         return bestValue;
+    }
+
+    private void markCell(Board board, int color, int cell) {
+        if (color == 1) {
+            board.mark(cell, getMark());
+        } else {
+            board.mark(cell, opponent_symbol());
+        }
     }
 
     private Mark opponent_symbol() {
@@ -70,5 +76,13 @@ public class PerfectComputer implements Player{
         if (board.getWinningMark() == opponent_symbol()) return (-10.0 / depth);
         if (board.isFull()) return 0;
         return 0;
+    }
+
+    private void findBestMove() {
+        for (Entry<Integer,Double> entry : scoredMoves.entrySet()) {
+            if (bestMove == null || entry.getValue().compareTo(bestMove.getValue()) > 0) {
+                bestMove = entry;
+            }
+        }
     }
 }
--- src/test/java/com/company/PerfectComputerTest.java
+++ src/test/java/com/company/PerfectComputerTest.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,22 +12,42 @@ import static org.junit.Assert.assertTrue;
 
 public class PerfectComputerTest {
 
+    private PerfectComputer tron;
+    private UserInterfaceSpy ui;
+    private Board board;
+
+
+    @Before
+    public void setUp() {
+        tron = new PerfectComputer(X);
+        ui = new UserInterfaceSpy();
+        board = new Board();
+    }
+
     @Test
     public void willChooseACornerIfBoardIsEmpty() {
-        PerfectComputer tron = new PerfectComputer(X);
-        UserInterfaceSpy ui = new UserInterfaceSpy();
-        Board board = new Board();
         assertTrue(Arrays.asList(0, 2, 6, 8).contains(tron.choosePosition(ui, board)));
     }
     
     @Test
     public void willBlockWinConditionFromOpponent() {
-        PerfectComputer tron = new PerfectComputer(X);
-        UserInterfaceSpy ui = new UserInterfaceSpy();
-        Board board = new Board();
         board.mark(0, O);
         board.mark(3, X);
         board.mark(2, O);
         assertEquals(1, tron.choosePosition(ui, board));
     }
+
+    @Test
+    public void willPlayCenterIfFirstPlayerTookCorner() {
+        board.mark(0, O);
+        assertEquals(4, tron.choosePosition(ui, board));
+    }
+
+    @Test
+    public void willWinGameIfOptionIsAvailable() {
+        board.mark(0, X);
+        board.mark(2, X);
+        assertEquals(1, tron.choosePosition(ui, board));
+    }
+
 }
