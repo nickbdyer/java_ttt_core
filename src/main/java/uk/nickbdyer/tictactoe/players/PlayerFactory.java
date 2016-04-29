e8db42ee321a478c01c2da7db76f987433300a71
Nick Dyer <nbdyer@gmail.com>
2016-04-29 11:08:40 +0100

    Modifies PlayerFactory to make slow computers, also modifies tests to match new instances

--- src/main/java/uk/nickbdyer/tictactoe/players/PlayerFactory.java
+++ src/main/java/uk/nickbdyer/tictactoe/players/PlayerFactory.java
@@ -2,31 +2,38 @@ package uk.nickbdyer.tictactoe.players;
 
 import uk.nickbdyer.tictactoe.GameType;
 import uk.nickbdyer.tictactoe.Player;
-import uk.nickbdyer.tictactoe.Mark;
+import uk.nickbdyer.tictactoe.delayers.ThreadDelayer;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static uk.nickbdyer.tictactoe.Mark.O;
+import static uk.nickbdyer.tictactoe.Mark.X;
 
 public class PlayerFactory {
 
     public List<Player> create(GameType type) {
         if (type == GameType.HvsH) {
-            return Arrays.asList(new Human(Mark.X), new Human(Mark.O));
+            return Arrays.asList(new Human(X), new Human(O));
         } else if (type == GameType.HvsAi) {
-            return Arrays.asList(new Human(Mark.X), new DumbComputer(Mark.O));
+            return Arrays.asList(new Human(X), slow(new DumbComputer(O)));
         } else if (type == GameType.AivsH) {
-            return Arrays.asList(new DumbComputer(Mark.X), new Human(Mark.O));
+            return Arrays.asList(slow(new DumbComputer(X)), new Human(O));
         } else if (type == GameType.AivsAi) {
-            return Arrays.asList(new DumbComputer(Mark.X), new DumbComputer(Mark.O));
+            return Arrays.asList(slow(new DumbComputer(X)), slow(new DumbComputer(O)));
         } else if (type == GameType.HvsPAi) {
-            return Arrays.asList(new Human(Mark.X), new PerfectComputer(Mark.O));
+            return Arrays.asList(new Human(X), slow(new PerfectComputer(O)));
         } else if (type == GameType.PAivsH) {
-            return Arrays.asList(new PerfectComputer(Mark.X), new Human(Mark.O));
+            return Arrays.asList(slow(new PerfectComputer(X)), new Human(O));
         } else if (type == GameType.PAivsPAi) {
-            return Arrays.asList(new PerfectComputer(Mark.X), new PerfectComputer(Mark.O));
+            return Arrays.asList(slow(new PerfectComputer(X)), slow(new PerfectComputer(O)));
         } else {
             throw new RuntimeException("That is not a game type");
         }
+    }
+
+    private Player slow(Player computer) {
+        return new DelayedComputer(computer, new ThreadDelayer(), 1000);
     }
 
 }
--- src/test/java/uk/nickbdyer/tictactoe/players/PlayerFactoryTest.java
+++ src/test/java/uk/nickbdyer/tictactoe/players/PlayerFactoryTest.java
@@ -21,41 +21,41 @@ public class PlayerFactoryTest {
     public void canCreateAHumanAndComputerPlayer() {
         List<Player> players = new PlayerFactory().create(GameType.HvsAi);
         assertTrue(players.get(0) instanceof Human);
-        assertTrue(players.get(1) instanceof DumbComputer);
+        assertTrue(players.get(1) instanceof DelayedComputer);
     }
 
     @Test
     public void canCreateAComputerAndHumanPlayer() {
         List<Player> players = new PlayerFactory().create(GameType.AivsH);
-        assertTrue(players.get(0) instanceof DumbComputer);
+        assertTrue(players.get(0) instanceof DelayedComputer);
         assertTrue(players.get(1) instanceof Human);
     }
 
     @Test
     public void canCreateTwoComputerPlayers() {
         List<Player> players = new PlayerFactory().create(GameType.AivsAi);
-        assertTrue(players.get(0) instanceof DumbComputer);
-        assertTrue(players.get(1) instanceof DumbComputer);
+        assertTrue(players.get(0) instanceof DelayedComputer);
+        assertTrue(players.get(1) instanceof DelayedComputer);
     }
 
     @Test
     public void canCreateAHumanAndPerfectComputerPlayer() {
         List<Player> players = new PlayerFactory().create(GameType.HvsPAi);
         assertTrue(players.get(0) instanceof Human);
-        assertTrue(players.get(1) instanceof PerfectComputer);
+        assertTrue(players.get(1) instanceof DelayedComputer);
     }
 
     @Test
     public void canCreateAPerfectComputerAndHumanPlayer() {
         List<Player> players = new PlayerFactory().create(GameType.PAivsH);
-        assertTrue(players.get(0) instanceof PerfectComputer);
+        assertTrue(players.get(0) instanceof DelayedComputer);
         assertTrue(players.get(1) instanceof Human);
     }
 
     @Test
     public void canCreateTwoPerfectComputerPlayers() {
         List<Player> players = new PlayerFactory().create(GameType.PAivsPAi);
-        assertTrue(players.get(0) instanceof PerfectComputer);
-        assertTrue(players.get(1) instanceof PerfectComputer);
+        assertTrue(players.get(0) instanceof DelayedComputer);
+        assertTrue(players.get(1) instanceof DelayedComputer);
     }
 }
