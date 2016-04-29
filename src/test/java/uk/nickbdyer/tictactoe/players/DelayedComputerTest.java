14cb2e70c106057aa3c39e8272eac559e5742582
Nick Dyer <nbdyer@gmail.com>
2016-04-29 11:21:03 +0100

    Refactors DelayedComputer and Human tests for better naming, and behaviour

--- src/test/java/uk/nickbdyer/tictactoe/players/DelayedComputerTest.java
+++ src/test/java/uk/nickbdyer/tictactoe/players/DelayedComputerTest.java
@@ -1,38 +1,53 @@
 package uk.nickbdyer.tictactoe.players;
 
+import org.junit.Before;
+import org.junit.Test;
 import uk.nickbdyer.tictactoe.Board;
 import uk.nickbdyer.tictactoe.UserInterface;
 import uk.nickbdyer.tictactoe.delayers.MockDelayer;
-import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-import static uk.nickbdyer.tictactoe.Mark.X;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static uk.nickbdyer.tictactoe.Mark.X;
 
 public class DelayedComputerTest {
 
-    @Test
-    public void canDelayDumbComputerPlayer() {
-        UserInterface ui = new UserInterface(new Scanner(""), new PrintStream(new ByteArrayOutputStream()));
-        Board board = new Board();
-        MockDelayer mockDelayer = new MockDelayer();
-        DumbComputer dumbComputer = new DumbComputer(X);
-        DelayedComputer delayedComputer = new DelayedComputer(dumbComputer,mockDelayer,0);
-        delayedComputer.choosePosition(ui, board);
-        assertTrue(mockDelayer.hasSleepBeenCalled);
+    private UserInterface ui;
+    private Board board;
+    private MockDelayer delayer;
+
+    @Before
+    public void setUp() {
+        ui = new UserInterface(new Scanner(""), new PrintStream(new ByteArrayOutputStream()));
+        board = new Board();
+        delayer = new MockDelayer();
     }
 
     @Test
-    public void canDelayPerfectComputerPlayer() {
-        UserInterface ui = new UserInterface(new Scanner(""), new PrintStream(new ByteArrayOutputStream()));
-        Board board = new Board();
-        MockDelayer mockDelayer = new MockDelayer();
-        PerfectComputer perfectComputer = new PerfectComputer(X);
-        DelayedComputer delayedComputer = new DelayedComputer(perfectComputer,mockDelayer,0);
+    public void hasMark() {
+        DumbComputer dumbComputer = new DumbComputer(X);
+        DelayedComputer delayedComputer = new DelayedComputer(dumbComputer, delayer, 0);
+        assertEquals(X, delayedComputer.getMark());
+
+    }
+
+    @Test
+    public void delaysDumbComputer() {
+        DumbComputer dumbComputer = new DumbComputer(X);
+        DelayedComputer delayedComputer = new DelayedComputer(dumbComputer, delayer, 0);
         delayedComputer.choosePosition(ui, board);
-        assertTrue(mockDelayer.hasSleepBeenCalled);
+        assertTrue(delayer.hasSleepBeenCalled);
+    }
+
+    @Test
+    public void delaysPerfectComputer() {
+        PerfectComputer perfectComputer = new PerfectComputer(X);
+        DelayedComputer delayedComputer = new DelayedComputer(perfectComputer, delayer, 0);
+        delayedComputer.choosePosition(ui, board);
+        assertTrue(delayer.hasSleepBeenCalled);
     }
 }
--- src/test/java/uk/nickbdyer/tictactoe/players/HumanTest.java
+++ src/test/java/uk/nickbdyer/tictactoe/players/HumanTest.java
@@ -1,33 +1,27 @@
 package uk.nickbdyer.tictactoe.players;
 
+import org.junit.Test;
 import uk.nickbdyer.tictactoe.Board;
 import uk.nickbdyer.tictactoe.UserInterface;
-import org.junit.Before;
-import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-import static uk.nickbdyer.tictactoe.Mark.X;
 import static org.junit.Assert.assertEquals;
+import static uk.nickbdyer.tictactoe.Mark.X;
 
 public class HumanTest {
 
-    private Human nick;
-
-    @Before
-    public void setUp() throws Exception {
-        nick = new Human(X);
-    }
-
     @Test
-    public void canRetrieveItsMark() {
+    public void hasMark() {
+        Human nick = new Human(X);
         assertEquals(X, nick.getMark());
     }
 
     @Test
-    public void canMarkTheBoard() {
+    public void returnsMoveChoice() {
+        Human nick = new Human(X);
         Board board = new Board();
         UserInterface ui = new UserInterface(new Scanner("1"), new PrintStream(new ByteArrayOutputStream()));
         assertEquals(0, nick.choosePosition(ui, board));
