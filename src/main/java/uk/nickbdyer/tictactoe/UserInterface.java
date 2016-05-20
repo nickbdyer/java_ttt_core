c198242c1aa0766c923827428f8f7dc0137861a2
Nick Dyer <nbdyer@gmail.com>
2016-05-20 14:48:05 +0100

    Updates UserInterface to show public UI methods

--- src/main/java/uk/nickbdyer/tictactoe/UserInterface.java
+++ src/main/java/uk/nickbdyer/tictactoe/UserInterface.java
@@ -1,4 +1,32 @@
 package uk.nickbdyer.tictactoe;
 
 public interface UserInterface {
+
+    int getNumber();
+
+    String getYorN();
+
+    GameType makeGameChoice();
+
+    void displayBoard(Board board);
+
+    void displayMoveInstructions();
+
+    void displayWinner(Board board);
+
+    void displayDraw();
+
+    void displayComputerThinking();
+
+    void displayGameChoiceMenu();
+
+    void displayInvalidInput();
+
+    void displayInvalidSelection();
+
+    void displayInvalidPosition();
+
+    void displayReplayQuery();
+
+    void displayResetNotice();
 }
